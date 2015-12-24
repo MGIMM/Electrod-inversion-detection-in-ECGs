6aa0c5bf597e9230c023e4835a48c0584f376742
MGIMM <duqiming2004@gmail.com>
2015-12-24 17:20:32 +0100

    finish naive fft

--- ECG_function.py
+++ ECG_function.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 #function for data importing
@@ -12,7 +13,16 @@ def DataImporting():
 #function for data sampling
 def DataSample(X,ID,index):
     'X:dataframe of input data'
-    'Id = id, from 0 to 4'
+    'Id = id, from 0 to 999'
     'index = index of channel, from 1 to 12'
     return X.iloc[ID,1+(index-1)*750:1+index*750] 
 
+#function for ploting
+def PlotAllChannels (X,ID):
+    fig, ((ax1, ax2, ax3), (ax4, ax5,ax6),(ax7, ax8, ax9), (ax10, ax11,ax12)) = plt.subplots(4, 3, sharex='col', sharey='row')
+    fig.set_size_inches(15, 15)
+    for (ax,i) in zip((ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10, ax11, ax12),range(1,13,1)):
+        ax.plot(DataSample(X,ID,i))
+        ax.set_title('channel '+str(i))
+    return True    
+    
